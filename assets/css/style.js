bdb4c98b3f25ee0ea51f8c065f7643c2c2846a61
Sharif Hadidi <sharifhadidi@gmail.com>
2017-03-25 23:48:27 -0400

    added files, and styles

--- assets/css/style.js
+++ assets/css/style.js
@@ -7,6 +7,35 @@ const {width, height, scale} = Dimensions.get("window"),
 
 export default StyleSheet.create({
     "body": {
-        "backgroundColor": "blue"
+        "marginTop": 0,
+        "marginRight": 0,
+        "marginBottom": 0,
+        "marginLeft": 0,
+        "paddingTop": 0,
+        "paddingRight": 0,
+        "paddingBottom": 0,
+        "paddingLeft": 0,
+        "width": "100%"
+    },
+    "header": {
+        "backgroundColor": "#252121",
+        "height": "15%"
+    },
+    "h2": {
+        "textAlign": "center",
+        "fontWeight": "bold"
+    },
+    "collection": {
+        "backgroundColor": "#322e2d"
+    },
+    "row": {
+        "width": "80%",
+        "marginTop": 0,
+        "marginRight": "auto",
+        "marginBottom": 0,
+        "marginLeft": "auto"
+    },
+    "inner-box": {
+        "backgroundColor": "purple"
     }
 });
