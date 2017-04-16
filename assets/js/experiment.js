a0dd78443896bf4811e40cfc4a4ba6bfbda5e6d0
estherhersh <estherhersh@gmail.com>
2017-04-16 14:32:14 -0400

    downloadEx again

--- assets/js/experiment.js
+++ assets/js/experiment.js
@@ -15,7 +15,7 @@
          });
       // currentValue= JSON.stringify(editor.getValue())
       // console.log(editor)
-      editorEx.setSize(700, window.innerHeight)
+      editorEx.setSize(850, window.innerHeight-100)
       // editorEx.setValue(currentValue);
       
       // editor.on("change", function() {
@@ -57,3 +57,83 @@
 
           }
   });
+/*
+ * DOWNLOADABLE TEXTAREA v1.0.0
+ *
+ * by Ian Marshall (ian.js.tutor@gmail.com), 2017
+ * ======
+ * Add the "data-downloadable" attribute to the textarea
+ * you want to download as text. The value of this attribute
+ * should be the filename and extension you want to use
+ * for the downloaded text.
+ *
+ * For example:
+ * <textarea data-downloadable="text.txt"></textarea>
+ *
+ * Then create a button with the exact same data-downloadable
+ * attribute. This way, you can have multiple textareas and
+ * buttons, as long as each pair has a different filename.
+ *
+ * For example:
+ * <button data-downloadable="text.txt">download</button>
+ *
+ * NOTE: some filename extensions (.js, .py, .php, etc.)
+ * may cause browsers to issue a warning, something like
+ * "This type of file can harm your computer. Do you want
+ * to keep this file anyway?"
+ *
+ * requirements: window and document objects, nothing else
+ * ======
+ */
+
+      ;(function(window, document){
+document.addEventListener("DOMContentLoaded", function(){
+    //no downloadable textareas?
+    if (!document.querySelector("[data-downloadable]")) return;
+//APP INIT:
+    //iterate through each downloadable textarea/button combination
+    [].forEach.call(document.querySelectorAll("button[data-downloadable]"), downloadableInit);
+//INIT EACH DOWNLOADABLE:
+    function downloadableInit(downloadButton){
+        //one textarea for each button, both with matching data-downloadable attributes
+        if (document.querySelectorAll("textarea[data-downloadable='" + downloadButton.getAttribute("data-downloadable") + "']").length !== 1) return;
+        //add event listener
+        downloadButton.addEventListener("click", downloadTextarea);
+    }
+//EVENT HANDLING:
+    function downloadTextarea(e){
+      var cdn = "<script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/p5.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/addons/p5.dom.min.js'></script><script>"
+      var endcdn="</script></body></html>"
+      var editorValue= editorEx.getValue();
+      code = cdn + editorValue + endcdn;
+        //get file name from button
+        var fileName = e.target.getAttribute("data-downloadable"),
+        //get text from matching textarea
+
+    //The textarea is not a normal textarea with codemirror so the value can be downloaded but not the textarea
+            // text = document.querySelector("textarea[data-downloadable='" + fileName + "']").value,
+          text = code,
+
+        //create blob from text
+            textBlob = new Blob([text], {type: "text/plain"}),
+        //create link that will only temporarily be attached to document
+            tempLink = document.createElement("a");
+        //add data to temporary link
+        tempLink.download = fileName;
+        tempLink.innerHTML = "file download";
+        window.URL = window.URL || window.webkitURL;
+        tempLink.href = window.URL.createObjectURL(textBlob);
+        //temporary link deletes itself on click
+        tempLink.onclick = function(e){
+            e.target.outerHTML = "";
+        }
+        //keep temporary link from view
+        tempLink.style.display = "none";
+        //attach temporary link to DOM
+        document.body.appendChild(tempLink);
+        //trigger its click event
+        tempLink.click();
+    }
+}); //end DOMContentLoaded handler
+})(window, document);
+
